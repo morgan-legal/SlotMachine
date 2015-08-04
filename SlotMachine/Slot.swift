523f75eeb084da91a2f19bb57cd9cab6d58b5f58
morgan-legal <legal.morgan@gmail.com>
2015-08-04 16:27:45 -0700

    Slot Machine Project finished

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Morgan Le Gal on 5/1/15.
+//  Copyright (c) 2015 Morgan Le Gal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
